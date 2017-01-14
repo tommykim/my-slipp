422af25be39b3a4a2cb24d8ca745774bfc4ebba4
tommykim <tommynara@gmail.com>
2017-01-14 21:08:14 +0900

    jrepath mody

---
+++ src/main/java/net/slipp/domain/CPU.java
@@ -0,0 +1,56 @@
+package net.slipp.domain;
+import java.awt.List;
+
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+public class CPU extends AbstractEntity{
+	@ManyToOne
+	@JsonProperty
+	private User writer;
+	
+	@JsonProperty
+	private String title;
+	
+	@Lob
+	@JsonProperty
+	private String contents;
+	
+	@JsonProperty
+	private Integer countOfAnswer = 0;
+		
+	public CPU() {}
+	
+	public CPU(User writer, String title, String contents) {
+		this.writer = writer;
+		this.title = title;
+		this.contents = contents;
+	}
+	
+	public void update(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
+
+	public boolean isSameWriter(User loginUser) {
+		System.out.println("writer : " + writer);
+		return this.writer.equals(loginUser);
+	}
+	
+	public void addAnswer() {
+		this.countOfAnswer += 1;
+	}
+	
+	public void deleteAnswer() {
+		this.countOfAnswer -= 1;
+	}
+	
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,17 @@
-INSERT INTO benchmark.user (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (1, 'admin', 'test', 'tommy', 'tommy@contactory.net', CURRENT_TIMESTAMP());
-INSERT INTO benchmark.user (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (2, 'master', 'test', 'master', 'master@contactory.net', CURRENT_TIMESTAMP());
-INSERT INTO benchmark.user (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (3, 'yomit', 'test', 'yomit', 'yomit@contactory.net', CURRENT_TIMESTAMP());
+INSERT INTO user (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (1, 'admin', 'test', 'tommy', 'tommy@contactory.net', CURRENT_TIMESTAMP());
+INSERT INTO user (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (2, 'master', 'test', 'master', 'master@contactory.net', CURRENT_TIMESTAMP());
+INSERT INTO user (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (3, 'yomit', 'test', 'yomit', 'yomit@contactory.net', CURRENT_TIMESTAMP());
 
-INSERT INTO benchmark.question (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'2017-1-1 Week Android App BenchMark Chart ','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/cpu.png>', CURRENT_TIMESTAMP(), 3);
-INSERT INTO benchmark.question (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 1,'2017-1-7 Week Android App BenchMark Chart','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/memory.png>', CURRENT_TIMESTAMP(), 0);
+INSERT INTO question (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'2017-1-1 Week Android App BenchMark Chart ','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/cpu.png>', CURRENT_TIMESTAMP(), 3);
+INSERT INTO question (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 1,'2017-1-7 Week Android App BenchMark Chart','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/memory.png>', CURRENT_TIMESTAMP(), 0);
 
-INSERT INTO benchmark.answer (id, create_date, modified_date , contents, question_id, writer_id) VALUES (1, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),'good performace!!',1,1);
-INSERT INTO benchmark.answer (id, create_date, modified_date , contents, question_id, writer_id) VALUES (2, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),'good performace!!',1,2);
-INSERT INTO benchmark.answer (id, create_date, modified_date , contents, question_id, writer_id) VALUES (3, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),'good performace!!',1,3);
+INSERT INTO answer (id, create_date, modified_date , contents, question_id, writer_id) VALUES (1, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),'good performace!!',1,1);
+INSERT INTO answer (id, create_date, modified_date , contents, question_id, writer_id) VALUES (2, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),'good performace!!',1,2);
+INSERT INTO answer (id, create_date, modified_date , contents, question_id, writer_id) VALUES (3, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),'good performace!!',1,3);
 
-INSERT INTO benchmark.cpu (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'2017-1-1 Week Android App CPU BenchMark Chart ','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/cpu.png>', CURRENT_TIMESTAMP(), 0);
-INSERT INTO benchmark.cpu (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 1,'2017-1-7 Week Android App CPU BenchMark Chart','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/memory.png>', CURRENT_TIMESTAMP(), 0);
+INSERT INTO cpu (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'2017-1-1 Week Android App CPU BenchMark Chart ','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/cpu.png>', CURRENT_TIMESTAMP(), 0);
+INSERT INTO cpu (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 1,'2017-1-7 Week Android App CPU BenchMark Chart','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/memory.png>', CURRENT_TIMESTAMP(), 0);
 
-INSERT INTO benchmark.battery (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'2017-1-1 Week Android App Battery BenchMark Chart ','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/cpu.png>', CURRENT_TIMESTAMP(), 0);
-INSERT INTO benchmark.battery (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 1,'2017-1-7 Week Android App Battery BenchMark Chart','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/memory.png>', CURRENT_TIMESTAMP(), 0);
+INSERT INTO battery (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'2017-1-1 Week Android App Battery BenchMark Chart ','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/cpu.png>', CURRENT_TIMESTAMP(), 0);
+INSERT INTO battery (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 1,'2017-1-7 Week Android App Battery BenchMark Chart','<img src=https://s3.ap-northeast-2.amazonaws.com/yomit/memory.png>', CURRENT_TIMESTAMP(), 0);
 
